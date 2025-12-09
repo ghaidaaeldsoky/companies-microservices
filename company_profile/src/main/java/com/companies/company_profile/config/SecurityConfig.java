521e408dc2f77a944bb795af129847ebc77d5fab
Ghaidaa.Elsayed <ghaidaaeldsoky@gmail.com>
2025-12-09 14:13:00 +0200

    added company profile security config #1

---
+++ company_profile/src/main/java/com/companies/company_profile/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.companies.company_profile.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return JwtDecoders.fromIssuerLocation("http://localhost:8180/realms/master");
+//    }
+}
--- company_profile/src/test/java/com/companies/company_profile/CompanyProfileApplicationTests.java
+++ company_profile/src/test/java/com/companies/company_profile/CompanyProfileApplicationTests.java
@@ -2,9 +2,14 @@ package com.companies.company_profile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class CompanyProfileApplicationTests {
+
+	@MockitoBean
+    JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
